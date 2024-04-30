c50577ab380202ab40f29ce655675baa58a61435
Harry-Cao <1052764449@qq.com>
2024-04-30 21:08:56 +0800

    update home page structure to MVVM

--- lib/main_container/home/home.dart
+++ lib/main_container/home/home.dart
@@ -1,68 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:gougouliu_flutter/main_container/home/model/home_post_model.dart';
 import 'package:gougouliu_flutter/main_container/home/view/home_post_cell.dart';
+import 'package:gougouliu_flutter/main_container/home/view_model/home_page_view_model.dart';
 import 'package:gougouliu_flutter/modules/post/post.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:provider/provider.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
-  State<StatefulWidget> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  List<HomePostModel> allPosts = [];
-
-  @override
-  void initState() {
-    super.initState();
-    fetchData();
-  }
-
-  void fetchData() async {
-    try {
-      final response =
-          await http.get(Uri.parse('http://192.168.0.211:8888/api/home/post'));
-      if (response.statusCode == 200) {
-        Map<String, dynamic> data = jsonDecode(response.body);
-        if (data["data"] != null) {
-          List<HomePostModel> posts = (data["data"] as List)
-              .map((e) => HomePostModel.fromJson(e))
-              .toList();
-          setState(() {
-            allPosts = posts;
-          });
-        }
-      } else {
-        throw Exception('Failed to load data');
-      }
-    } catch (e) {
-      debugPrint(e.toString());
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home"),
-      ),
-      body: MasonryGridView.count(
-        crossAxisCount: 2,
-        itemCount: allPosts.length,
-        itemBuilder: (context, index) {
-          final postModel = allPosts[index];
-          return HomePostCell(
-            postModel: postModel,
-            onTap: () => Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => PostPage(postModel: postModel))),
-          );
-        },
+    return ChangeNotifierProvider<HomePageViewModel>(
+      create: (_) => HomePageViewModel()..fetchData(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Home"),
+        ),
+        body: Consumer<HomePageViewModel>(
+          builder: (context, viewModel, child) {
+            return MasonryGridView.count(
+              crossAxisCount: 2,
+              itemCount: viewModel.allPosts.length,
+              itemBuilder: (context, index) {
+                final postModel = viewModel.allPosts[index];
+                return HomePostCell(
+                  postModel: postModel,
+                  onTap: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => PostPage(postModel: postModel),
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
--- lib/main_container/home/view_model/home_page_view_model.dart
+++ lib/main_container/home/view_model/home_page_view_model.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:gougouliu_flutter/main_container/home/model/home_post_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class HomePageViewModel extends ChangeNotifier {
   List<HomePostModel> allPosts = [];
+
+  Future<void> fetchData() async {
+    try {
+      final response =
+          await http.get(Uri.parse('http://192.168.0.211:8888/api/home/post'));
+      if (response.statusCode == 200) {
+        Map<String, dynamic> data = jsonDecode(response.body);
+        if (data["data"] != null) {
+          List<HomePostModel> posts = (data["data"] as List)
+              .map((e) => HomePostModel.fromJson(e))
+              .toList();
+          allPosts = posts;
+          notifyListeners();
+        }
+      } else {
+        throw Exception('Failed to load data');
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
+  }
 }
